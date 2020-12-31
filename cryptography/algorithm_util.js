07f132e65900df2fd1c48586cea671f21f2ac385
Willian Mascimiano <willian@MacBook-Air-de-Willian.local>
2020-12-30 23:53:55 -0300

    Util file

---
+++ cryptography/algorithm_util.js
@@ -0,0 +1,10 @@
+
+const alphabet = [
+    "A","B","C","D","E","F","G","H","I","J","K","L",
+    "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+  ];
+  
+
+const englishDictionary = "THIS IS A COMMON TEXT WITH THE MOST USED WORDS IN ENGLISH. CAN WE SEE TOO THIS TEXT"
++" THAT WILL HELP HAPPY NEW YEAR";
+//englishDictionary.match(["YEAR"])
